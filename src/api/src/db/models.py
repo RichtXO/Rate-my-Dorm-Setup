3155f1950d4c4fe2249d994b4be4876f2a47fc3f
Liam Haining <ljhaining@gmail.com>
2021-10-21 14:03:00 -0400

    started database models

--- src/api/src/db/models.py
+++ src/api/src/db/models.py
@@ -1,4 +1,38 @@
 from tortoise import fields, models
+from tortoise.contrib.pydantic import pydantic_model_creator
 
+class Users(models.Model):
+    username = fields.CharField(max_length=125, unique=True, pk=True)
+    email = fields.CharField(max_length=125, unique=True)
+    password = fields.CharField(max_length=125, null=True)
 
-# TODO Models in here!
+class Follows(models.Model):
+    id = fields.IntField(pk=True)
+    follower = fields.ForeignKeyField("models.Users", related_name="follow")
+    following = fields.ForeignKeyField("models.Users", related_name="follower")
+
+class Posts(models.Model):
+    id = fields.IntField(pk=True)
+    posted_by = fields.ForeignKeyField("models.Users", related_name="poster")
+    title = fields.CharField(max_length=75)
+    description = fields.TextField()
+    posted_at = fields.DatetimeField(auto_now_add=True)
+
+class Comments(models.Model):
+    id = fields.IntField(pk=True)
+    postid = fields.ForeignKeyField("models.Posts", related_name="comment")
+    posted_by = fields.ForeignKeyField("models.Users", related_name="commenter")
+    text = fields.TextField()
+    posted_at = fields.DatetimeField(auto_now_add=True)
+
+class Ratings(models.Model):
+    id = fields.IntField(pk=True)
+    rated_by = fields.ForeignKeyField("models.Users", related_name="rate")
+    postid = fields.ForeignKeyField("models.Posts", related_name="ratingid")
+    ratingvalue = fields.IntField()
+
+User_Pydantic = pydantic_model_creator(Users, name="User")
+Follow_Pydantic = pydantic_model_creator(Follows, name="Follow")
+Post_Pydantic = pydantic_model_creator(Posts, name="Post")
+Comment_Pydantic = pydantic_model_creator(Comments, name="Comment")
+Rating_Pydantic = pydantic_model_creator(Ratings, name="Rating")
--- src/api/src/main.py
+++ src/api/src/main.py
@@ -1,6 +1,15 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware 
+from typing import List
 
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+
+from src.db.models import User_Pydantic, Users
+from pydantic import BaseModel
+
+from src.db.register import register_tortoise
+from src.db.config import TORTOISE_ORM
+
+from tortoise.contrib.fastapi import HTTPNotFoundError, register_tortoise
 
 app = FastAPI()
 
@@ -12,6 +21,32 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/")
-def home():
-    return "Hello, World!"
+app = FastAPI(title="Tortoise ORM FastAPI example")
+
+#register_tortoise(app, config=TORTOISE_ORM, generate_schemas=False)
+
+register_tortoise(
+    app,
+    db_url="sqlite://:memory:",
+    modules={"models": ["src.db.models"]},
+    generate_schemas=True,
+    add_exception_handlers=True,
+)
+
+class Status(BaseModel):
+    message: str
+
+@app.get("/users", response_model=List[User_Pydantic])
+async def get_users():
+    return await User_Pydantic.from_queryset(Users.all())
+
+@app.get(
+    "/user/{user_name}", response_model=User_Pydantic, responses={404: {"model": HTTPNotFoundError}}
+)
+async def get_user(user_name: str):
+    return await User_Pydantic.from_queryset_single(Users.get(username=user_name))
+
+@app.post("/users", response_model=User_Pydantic)
+async def create_user(user: User_Pydantic):
+    user_obj = await Users.create(**user.dict(exclude_unset=True))
+    return await User_Pydantic.from_tortoise_orm(user_obj)
